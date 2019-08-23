3fa098adef251e0e0842391eb09f754a9ce506c6
myungho <skqoaudgh2@hanmail.net>
2019-08-23 15:06:55 +0900

    Rest API 라우터 삭제.

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const authController = require('../controllers/auth');
-
-router.post('/', authController.loginUser);
-
-module.exports = router;
